d838038c294bc2f75a030cf98e01a5c97907e3ce
moria <moria543210@gmail.com>
2025-01-05 19:37:32 +0200

    סיימתי את קומפיוט פורם ואיז פורם ועכשיו אני מתחילה לעשות עליהן טסטים

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(SCell.computForm("15+(2+3)*4"));
     }
 }
 
--- src/SCell.java
+++ src/SCell.java
@@ -1,12 +1,10 @@
 // Add your documentation below:
 
-import java.sql.Array;
-
 public class SCell implements Cell {
     private String line;
     private int type;
     public boolean vSf (String s){// valid signs for form beside "="
-      char [] validSigns  = {'A','B','C','D','E','F','G','H','I','1','2','3','4','5','6','7','8','9','0',' ','+','-','*','/','(',')'};
+        char [] validSigns  = {'A','B','C','D','E','F','G','H','I','1','2','3','4','5','6','7','8','9','0',' ','+','-','*','/','(',')'};
         for (int i = 1; i<s.length();i++){
             int valid = 0;
             for (int j = 0;j< validSigns.length;j++){
@@ -20,75 +18,125 @@ public class SCell implements Cell {
         return true;
     }
 
-public boolean isNumber(String sCell){ // "1234"
-    try {
-        double num = Double.parseDouble(sCell);
+    public static boolean isNumber(String sCell){ // "1234"
+        try {
+            double num = Double.parseDouble(sCell);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public boolean isForm(String sCell){
+        if (sCell.charAt(0)!='=')
+            return false;
+        if (!vSf(sCell.substring(1)))
+        return false;
+        try {
+            computForm(sCell);
+        } catch (Exception e){
+            return false;
+
+        }
         return true;
     }
-    catch (Exception e) {
-        return false;
-    }
-}
-public Double simpEval(String s,char[] arr){                        // this function is for calculating a form on the right order
-       for()
-}
-
-
-
-
-
-
-public boolean isForm(String sCell){
-    if (sCell.charAt(0)!='='||vSf(sCell.substring(1)))
-        for (int i = 0;i<sCell.length();i++){
+    public  static double computForm(String form){
+        if (form.charAt(0)=='=')
+            form= form.substring(1);
+        if (isNumber(form)){
+            return Double.parseDouble(form);
         }
-        return false;
-}
-public double eval(String form){
-        if (isNumber(form.substring(1))){
-            return Double.parseDouble(form.substring(1));
-    }
-    if (isNumber(form)) {
-        return Double.parseDouble(form);
-    }
         int count;
         int countRound =0;
         int indOArr = 0;
-        char [] op = new char[form.length()-2];
-        int[] num = new int[form.length()-2];
-        for (int i = 1; i<form.length();i++){
-            if(form.charAt(i)=='(')
-                countRound++;
-            if (form.charAt(i)==')')
-                countRound--;
-                  if (form.charAt(i)=='+'||form.charAt(i)=='-'){
-                      num[indOArr]=1+countRound;
-                      op[indOArr]=form.charAt(i);
-                      indOArr++;
-                  }
-                    if (form.charAt(i)=='*'||form.charAt(i)=='/'){
-                        num[indOArr]=2+countRound;
-                        op[indOArr]=form.charAt(i);
-                        indOArr++;
+        char [] op = new char[form.length()-1];
+        int[] valueop = new int[form.length()-1];
+        int[] indOfop = new int[form.length()-1];
+        if (form.contains("(")){
+            int firstRou=-1;
+            int lastRou=-1;
+            for (int i = 0; i<form.length();i++){
+                if(form.charAt(i)=='(') {
+                    if (countRound==0){
+                        firstRou=i;
                     }
-            }
-        for (int i = 0;i<indOArr-1;i++){
-            for (int j = 0;j<indOArr-1-i;j++){
-                if (num[j]<num[j+1]){
-                    int temp = num[j];
-                    num[j] = num[j+1];
-                    num[j+1] = temp;
-                    char swich = op[j];
-                    op[j] = op[j+1];
-                    op[j+1] = swich;
+                    countRound++;
+                }
+                if (form.charAt(i)==')') {
+                    countRound--;
+                    if (countRound==0&&firstRou!=-1){
+                        lastRou=i;
+                        break;
+                    }
                 }
             }
+            if (firstRou==0&& lastRou== form.length()-1){
+                return computForm(form.substring(1,form.length()-1));
+            }
+              double insidround = computForm(form.substring(firstRou+1,lastRou));
+           form = form.substring(0,firstRou)+Double.toString(insidround)+form.substring(lastRou+1,form.length());
+
+
+
+        }
+        for (int i = 0;i<form.length();i++){
+
+        if (form.charAt(i)=='+'||form.charAt(i)=='-'){
+            valueop[indOArr]=1;
+            op[indOArr]=form.charAt(i);
+            indOfop[indOArr]=i;
+            indOArr++;
+        }
+        if (form.charAt(i)=='*'||form.charAt(i)=='/'){
+            valueop[indOArr]=2;
+            op[indOArr]=form.charAt(i);
+            indOfop[indOArr]=i;
+            indOArr++;
         }
 
 
+        }
+        for (int i = 0;i<indOArr-1;i++){
+            for (int j = 0;j<indOArr-1-i;j++) {
+                if (valueop[j] < valueop[j + 1]) {
+                    int temp = valueop[j];
+                    valueop[j] = valueop[j + 1];
+                    valueop[j + 1] = temp;
+                    char swich = op[j];
+                    op[j] = op[j + 1];
+                    op[j + 1] = swich;
+                    int switchindop =indOfop[j];
+                    indOfop[j]=indOfop[j+1];
+                    indOfop[j+1]= switchindop;
+                }
+            }
+        }
+        if(indOArr==1&& !form.contains("(")){
+            double firstNum = Double.parseDouble(form.substring(0,indOfop[0]));
+            double secNum = Double.parseDouble(form.substring(indOfop[0]+1));
+            if (op[0]=='+')
+                return firstNum+secNum;
+                if (op[0]=='-')
+                    return firstNum-secNum;
+                    if (op[0]=='*')
+                        return firstNum*secNum;
+                        if (op[0]=='/')
+                          return  firstNum/secNum;
+        }
+        double first=computForm(form.substring(0,indOfop[indOArr-1]));
+        double second = computForm(form.substring(indOfop[indOArr-1]+1));
+        return  computForm(String.valueOf(first)+form.charAt(indOfop[indOArr-1])+String.valueOf(second));
 
-
-}
+    }
     // Add your code here
 
 
@@ -112,7 +160,7 @@ public double eval(String form){
     }
 
     @Override
-public void setData(String s) {
+    public void setData(String s) {
         // Add your code here
         line = s;
         /////////////////////
